cd32039cf093dc6d551e11fd2ec16a2667f4ce28
Fabiano <fabiano.ssantos79@gmail.com>
2023-04-20 03:19:40 -0300

    update system

--- To-Do/src/App.tsx
+++ To-Do/src/App.tsx
@@ -1,8 +1,41 @@
+import { useState } from 'react'
 import * as C from './App.styles'
+import {Item} from './types/Item'
+import { ListItem } from './components/ListItem'
+import { AddArea } from './components/AddArea'
 
 const App = () =>{
-  return(
 
+  const [list, setList] = useState<Item[]>([
+    {id:1, name:'Comprar leite na padaria', done: false},
+    {id:2, name:'Comprar um bolo no mercado', done: false}
+  ])
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list]
+    newList.push({
+      id:list.length + 1,
+      name: taskName,
+      done: false
+    })
+    setList(newList)
+  }
+
+  return(
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+
+        {/* Área de adicionar nova tarefa */}
+        <AddArea onEnter={handleAddTask}/>
+
+        {/* Lista de tarefas */}
+        {list.map((item, index) => (
+           <ListItem key={index} item={item}/> 
+        ))}
+
+      </C.Area>
+    </C.Container>
   )
 }
 
